f1d1380342ebbb107b07c987030e07ccdc7417f9
kevin kamla <kvnkamla4@gmail.com>
2023-04-01 13:26:23 +0100

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        //Role::create(['name'=>'organisme']);
+        //Role::create(['name'=>'admin']);
+        //Role::create(['name'=>'vacancier']);
+        return view('home');
+    }
+}
